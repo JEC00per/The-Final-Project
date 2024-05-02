f7f35aaa61369db1e4c97916aa51ce0bbc71378c
Joey Cooper <cactus234@icloud.com>
2024-05-02 15:37:36 -0500

    Imported time and random methods required for the timing and random chance portions of the program to function properly, defined the crew variable, and created the game introduction function

--- pirate_adventure.py
+++ pirate_adventure.py
@@ -1,1 +1,11 @@
+import time
+import random
 
+# Define global variables
+crew = ["Captain Blackbeard", "First Mate Red", "Navigator Grey", "Cook Brown", "Gunman Blue"]
+
+# Introduction to the game
+def introduction():
+    print("Welcome aboard, ye scurvy dogs!")
+    print("I'm Captain Blackbeard, and we're on a grand adventure to find the legendary pirate treasure!")
+    print("Arrr! Let's set sail and see what the seas have in store for us!")
